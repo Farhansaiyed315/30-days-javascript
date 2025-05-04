c04a9c1844f83ee91f71c25ffcdbbad9d13ee437
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-05-04 08:09:46 +0530

    Exercise Level one done

---
+++ Exercises/Objects/Objects.js
@@ -0,0 +1,19 @@
+console.log("Objects Level One");
+
+const dog = {
+    naam: "Dogesh bhai",
+    Legs: "Four",
+    color: "Black",
+    Age : "abhi jawan hai",
+    Breed:[]
+
+};
+// console.log(dog);
+
+dog.Breed.push("Desi")
+
+
+console.log(dog);
+
+
+
