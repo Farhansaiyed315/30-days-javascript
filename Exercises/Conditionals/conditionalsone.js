1d6750446701396351998fbbd3817dfa1ee6b8da
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-05-20 22:43:06 +0530

    Conditional Exercises added

---
+++ Exercises/Conditionals/conditionalsone.js
@@ -0,0 +1,6 @@
+//! Conditionals exercise page 74 
+console.log('Question number one ');
+
+//! Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+
