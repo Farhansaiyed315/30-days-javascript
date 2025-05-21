9b81fe60aa8730ddf9f9296645fd8a244e14f623
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-05-21 12:57:09 +0530

    Conditionsl Exercise

--- Exercises/Conditionals/conditionalsone.js
+++ Exercises/Conditionals/conditionalsone.js
@@ -3,4 +3,24 @@ console.log('Question number one ');
 
 //! Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 
+console.log('Question Number 1 = index.html');
+console.log('Question Number 2 = yourage.html');
 
+//! If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+//! o using if else o ternary operator.
+//! let a = 4 let b = 3
+console.log('Question number 3... ');
+let a = 4;
+let b = 3;
+
+
+
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("a is less than b");
+}
+
+console.log(a > b ? "a is greater than b" : "a is less than b");
+
+console.log('Question Number 4 = Even.html......');
