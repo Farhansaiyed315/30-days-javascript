7ca7619cc25fda85121307067e110aab5179dae1
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-05-25 09:56:10 +0530

    closures level 1 excercise soln

--- Exercises/Closures/Closures.js
+++ Exercises/Closures/Closures.js
@@ -1,1 +1,19 @@
-console.log();
+console.log('Closures level one exercise page no 243 of notes pdf.');
+
+//! Create a closure which has one inner function.
+console.log('Question number 1 level one ......')
+
+function outerFunction() {
+  let name = "Farhan";
+
+  function innerFunction() {
+    console.log("Hello", name);  // this 'name' is from outerFunction
+  }
+
+  return innerFunction;
+}
+
+const greet = outerFunction(); // outerFunction khud ko nahi balki inner function ko return kr raha hai
+                             //or you can say outer funciton is returning inner function innerFunction
+greet(); 
+
