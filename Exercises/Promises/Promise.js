02a31affd5cbea80d69982247f639346e6713d95
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-05-26 21:19:22 +0530

    Promises basics

--- Exercises/Promises/Promise.js
+++ Exercises/Promises/Promise.js
@@ -1,1 +1,27 @@
 console.log();
+
+
+console.log("promises");
+
+const PromiseOne = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log('Async is completed');
+        resolve(); // resolve the promise after 2 seconds
+    }, 2000);
+});
+
+// Consume the promise
+PromiseOne.then(function () {
+    console.log("Promise consumed");
+});
+
+
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
+console.log("Async task two");
+resolve()
+    }, 2000)
+}) .then(function(){
+    console.log("Async two resolved");
+    
+})
